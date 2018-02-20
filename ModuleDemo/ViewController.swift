dd3dfd331d0274b147477b1bf8beb4be0937c5c3
Alexiuce <alexiuce@qq.com>
2018-02-20 11:41:11 +0800

    项目初始化

---
+++ ModuleDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ModuleDemo
+//
+//  Created by Alexcai on 2018/2/20.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
