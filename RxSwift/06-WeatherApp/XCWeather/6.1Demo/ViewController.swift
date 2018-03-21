4c60048b583276b6dd90e02ec29680e5dfb2c377
Alexiuce <alexiuce@qq.com>
2018-03-21 09:29:10 +0800

    add bind to label

--- RxSwift/06-WeatherApp/XCWeather/6.1Demo/ViewController.swift
+++ RxSwift/06-WeatherApp/XCWeather/6.1Demo/ViewController.swift
@@ -11,15 +11,27 @@ import RxSwift
 import RxCocoa
 
 class ViewController: UIViewController {
+    @IBOutlet weak var cityLabel: UILabel!
     
     let disposeBag = DisposeBag()
+    var cityName = Variable("")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        ApiController.share.currentWeather(city: "Beijing").subscribe(onNext: { (weather) in
-            print(weather.cityName)
-            print(Thread.current)
-        }).disposed(by: disposeBag)
+        
+        
+//        cityName.asObservable().subscribe(onNext: {[weak self] in
+//            print($0)
+//           self?.cityLabel.text = $0
+//        }).disposed(by: disposeBag)
+        
+//        cityName.asObservable().bind(to: cityLabel.rx.text).disposed(by: disposeBag)
+//        cityName.value = "Hangzhou"
+        
+       let w = ApiController.share.currentWeather(city: "hc")
+        w.flatMap {
+           return Observable.of($0.cityName)
+        }.bind(to: cityLabel.rx.text).disposed(by: disposeBag)
     }
 
 
