4f1b630d174bf96e60a8d0c82009e8f369773f9c
Alexiuce <alexiuce@qq.com>
2018-03-28 17:07:59 +0800

    add jscore swift

---
+++ 01- iOS-love-js/01-iOS-JS/01-iOS-JS/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  01-iOS-JS
+//
+//  Created by caijinzhu on 2018/3/28.
+//  Copyright © 2018年 alexiuce.github.io. All rights reserved.
+//
+
+import UIKit
+
+import JavaScriptCore
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+    }
+
+}
+
---
+++ 01- iOS-love-js/01-iOS-JS/01-iOS-JSTests/_1_iOS_JSTests.swift
@@ -0,0 +1,53 @@
+//
+//  _1_iOS_JSTests.swift
+//  01-iOS-JSTests
+//
+//  Created by caijinzhu on 2018/3/28.
+//  Copyright © 2018年 alexiuce.github.io. All rights reserved.
+//
+
+import XCTest
+
+import JavaScriptCore
+
+class _1_iOS_JSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    func testJsDemo()  {
+        let context = JSContext()!
+        func sum(a: Int, b: Int) -> Int {
+            return a + b
+        }
+       
+    
+        context.setObject(sum, forKeyedSubscript: "mySum" as NSCopying & NSObjectProtocol)
+        
+        let s = context.evaluateScript("mySum(1,2)")
+        print(s?.toInt32() ?? "no value")
+        
+        
+        
+    }
+    
+}
