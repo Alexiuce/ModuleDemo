a88f7e55a0efe8bea9a3465f4e1fd14fb5be6a63
Alexiuce <alexiuce@qq.com>
2018-04-03 14:36:02 +0800

    add httpserver & filesystme

--- 02-Node.js/03-nodeApiExample/HTTPServer.js
+++ 02-Node.js/03-nodeApiExample/HTTPServer.js
@@ -1,22 +1,17 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer( (request, response) => {
 
-  switch (request.url){
-    case '/home':
-      response.write("home...")
-      break
-    case '/index':
-      response.write("index...")
-      break
-    case 'node':
-      response.write("node...")
-      break
-    default:
-     response.write("default...")
-  }
+  fs.readFile('./www/index.html',(err,data)=>{
+    "use strict";
 
-  const s = "for my string"
-  response.end(`hello node js ${s}`)
+    response.write(data)
+
+    const s = "for my string"
+    response.end(`hello node js ${s}`)
+
+  })
+
 });
 server.listen(3333)
