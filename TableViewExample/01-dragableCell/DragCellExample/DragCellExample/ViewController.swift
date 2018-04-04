b547766a0e38dcd24bdfa135215cbe95405cdd04
Alexiuce <alexiuce@qq.com>
2018-04-04 17:04:17 +0800

    add drag cell in one section

--- TableViewExample/01-dragableCell/DragCellExample/DragCellExample/ViewController.swift
+++ TableViewExample/01-dragableCell/DragCellExample/DragCellExample/ViewController.swift
@@ -10,16 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    fileprivate var sectionArray = ["One","Two","Three","Four","Five","Six"]
+    fileprivate var itemArray = ["name","job","fav","age"]
+    fileprivate var currentSelectedGroupIndex = -1
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.sectionFooterHeight = 0
+       
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+
+extension ViewController: UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionArray.count
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       
+        let cell = tableView.dequeueReusableCell(withIdentifier: "drag", for: indexPath)
+        
+        cell.textLabel?.text = "Group \(sectionArray[indexPath.section]) - index \(itemArray[indexPath.row])"
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Group \(sectionArray[section])"
+    }
+
+    
+}
+
+extension ViewController: UITableViewDelegate{
+   
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        sectionArray.swapAt(sourceIndexPath.section, destinationIndexPath.section)
+        itemArray.swapAt(sourceIndexPath.row, destinationIndexPath.row)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        currentSelectedGroupIndex = indexPath.section
+        tableView.isEditing = true
+        
+    }
+    
+    func tableView(_ tableView: UITableView, targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath, toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
+        if proposedDestinationIndexPath.section == sourceIndexPath.section {
+            return proposedDestinationIndexPath
+        }
+        return sourceIndexPath
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+       
+        return indexPath.section == currentSelectedGroupIndex
+    }
+  
+    
+    
+}
