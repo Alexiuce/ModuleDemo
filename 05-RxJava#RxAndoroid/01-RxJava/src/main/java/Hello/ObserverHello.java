54a6d9889a58b0502f85e4a4787fb34f69a74569
Alexiuce <alexiuce@qq.com>
2018-04-22 21:14:31 +0800

    add example

--- 05-RxJava#RxAndoroid/01-RxJava/src/main/java/Hello/ObserverHello.java
+++ 05-RxJava#RxAndoroid/01-RxJava/src/main/java/Hello/ObserverHello.java
@@ -2,6 +2,8 @@ package Hello;
 
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
 
@@ -10,6 +12,10 @@ public class ObserverHello {
 
     public static void main(String[] args) {
 
+        example1();
+    }
+
+    private static void example(){
         Observable<String> ob = Observable.just("one","two","three");
 
         Observer<String> sub = new Observer<String>() {
@@ -37,6 +43,38 @@ public class ObserverHello {
 
 
         ob.subscribe(sub);
+    }
 
+    private static void example1(){
+        Observable<String> obs = Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
+                emitter.onNext("this is test");
+            }
+        });
+        Observer<String> looker = new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+
+                System.out.println("next : " + s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+
+        obs.subscribe(looker);
     }
 }
