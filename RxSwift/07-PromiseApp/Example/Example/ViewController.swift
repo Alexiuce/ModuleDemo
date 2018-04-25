2082dcfd8134f210aa8011634429c6287238395d
Alexiuce <alexiuce@qq.com>
2018-04-25 10:18:14 +0800

    add PromiseKit example

--- RxSwift/07-PromiseApp/Example/Example/ViewController.swift
+++ RxSwift/07-PromiseApp/Example/Example/ViewController.swift
@@ -15,17 +15,42 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        getApi().then { (s) -> Thenable in
-            
-            } as! (_) -> _
         
-        // Do any additional setup after loading the view.
+        firstly {
+            getApi()
+            }.then { (apiResult) -> Promise<String> in
+                print(apiResult)
+                return self.getWeather(latitude: 23.0, longitude: 33.5)
+            }.done { (fulfill) in
+                print(fulfill)
+            }.catch { (e) in
+                print(e.localizedDescription)
+        }
+        
+        
+        
     }
     
     override var representedObject: Any? {
         didSet {
             // Update the view, if already loaded.
         }
+    }
+    
+    
+    fileprivate func example1(){
+        getApi()
+            .then { apiResult -> Promise<String> in
+                print(apiResult)
+                return self.getWeather(latitude: 23.3, longitude: 33.2)
+            }
+            .done {
+                print($0)
+            }.catch { (e) in
+                print(e.localizedDescription)
+            
+        }
+
     }
     
     
@@ -36,9 +61,33 @@ extension ViewController{
     fileprivate func getApi() -> Promise<String> {
         
         return Promise{ cb in
-            cb.fulfill("result")
+            cb.fulfill("result======")
             }
         
+    }
+    
+    func getWeather(latitude: Double, longitude: Double) -> Promise<String> {
+        return Promise { cb in
+            let urlString = "http://httpbin.org/get?lat=\(latitude)&lon=\(longitude)"
+            let url = URL(string: urlString)!
+            let request = URLRequest(url: url)
+            
+            let session = URLSession.shared
+            let dataTask = session.dataTask(with: request) { data, response, error in
+                if let data = data,
+                    let json = (try? JSONSerialization.jsonObject(with: data, options: [])) as? [String: Any]{
+                    cb.fulfill(json.description)
+                    
+                } else if let error = error {
+                    cb.reject(error)
+                    
+                } else {
+                    let error = NSError(domain: "PromiseKitTutorial", code: 0,userInfo: [NSLocalizedDescriptionKey: "Unknown error"])
+                    cb.reject(error)
+                }
+            }
+            dataTask.resume()
+        }
     }
     
     
