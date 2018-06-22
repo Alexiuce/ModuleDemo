87a3e0cd3bc0182b2dba58615d12d8ab41055f6d
Alexiuce <alexiuce@qq.com>
2018-06-22 10:45:42 +0800

    moduleDemo add thread target

---
+++ 01-Thread/main.swift
@@ -0,0 +1,20 @@
+//
+//
+// main.swift
+// Created by Alex on 2018/6/22
+// Copyright © 2018年 Alexcai. All rights reserved.
+
+//
+
+import Foundation
+
+print("Hello, World!")
+
+func pthread_demo()  {
+    print("p....")
+    pthread_create(<#T##UnsafeMutablePointer<pthread_t?>!#>, <#T##UnsafePointer<pthread_attr_t>?#>, <#T##(UnsafeMutableRawPointer) -> UnsafeMutableRawPointer?#>, <#T##UnsafeMutableRawPointer?#>)
+}
+
+pthread_demo()
+
+
