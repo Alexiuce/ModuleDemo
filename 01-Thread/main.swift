b66e2fedf2d163d9bedb19bc5ac01cdad0c37757
Alexiuce <alexiuce@qq.com>
2018-06-25 13:29:21 +0800

    thread state

--- 01-Thread/main.swift
+++ 01-Thread/main.swift
@@ -10,36 +10,31 @@ import Foundation
 
 class Person: NSObject {
    @objc func sayHello()  {
-        print("hello ....")
+        print("hello ....\(Thread.current)")
     }
 }
 
 
-print("Hello, World!")
 
 func pthread_demo()  {
-    print("p....")
+    
     
     var pt: pthread_t?
     var arg = "abc"
-    
+    /** tid = 0 线程创建成功 ,否则创建失败 */
    let tid = pthread_create(&pt, nil, { (sp) -> UnsafeMutableRawPointer? in
         let str = sp.assumingMemoryBound(to: String.self)
 
         print("create == \(str.pointee) \(Thread.current)")
         return nil
     }, &arg)
-    
-    
-    
-    print("\(tid) ")
-
+    print(tid)
 }
 func nsthread_demo(){
-    
+    /** 线程的状态: 新建---> 就绪 ---> 执行--->阻塞--->死亡 */
     let p = Person()
     let t = Thread(target:p, selector: #selector(p.sayHello), object: nil)
-
+    t.name = "mythread"
     t.start()
     
 }
