b55b3601b575ba5dc36a8011bba2d944ec5556ca
Alexiuce <alexiuce@qq.com>
2018-07-16 08:31:34 +0800

    add XCWeibo project

---
+++ 06-XCWeibo/06-XCWeibo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  06-XCWeibo
+//
+//  Created by Alexcai on 2018/7/16.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
