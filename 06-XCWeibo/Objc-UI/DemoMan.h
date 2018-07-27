7eabef31851d98a02f01d52eb58dd63619d4eeeb
Alexiuce <alexiuce@qq.com>
2018-07-27 08:48:49 +0800

    oc ({}) demo

---
+++ 06-XCWeibo/Objc-UI/DemoMan.h
@@ -0,0 +1,15 @@
+//
+//  DemoMan.h
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/7/27.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DemoMan : NSObject
+
+@property (nonatomic, copy) NSString *name;
+
+@end
---
+++ 06-XCWeibo/Objc-UI/DemoMan.m
@@ -0,0 +1,23 @@
+//
+//  DemoMan.m
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/7/27.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+#import "DemoMan.h"
+
+@implementation DemoMan
+
+
+
+
+- (void)demo{
+    /*  {} 表示作用域包装, ()表示执行*/
+    
+    // ({}) 最后一行为返回值
+    
+}
+
+@end
