264917467d96e82574f0e38de841ea535b128f57
Alexiuce <alexiuce@qq.com>
2018-07-31 13:54:03 +0800

    page view controller demo

---
+++ 06-XCWeibo/Objc-UI/PageViewController/XCPageViewController.h
@@ -0,0 +1,16 @@
+//
+//  XCPageViewController.h
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/7/31.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XCPageViewController : UIViewController
+
++ (instancetype)pageViewControllers:(NSArray <UIViewController *>*)children;
+
+
+@end
---
+++ 06-XCWeibo/Objc-UI/PageViewController/XCPageViewController.m
@@ -0,0 +1,32 @@
+//
+//  XCPageViewController.m
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/7/31.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+#import "XCPageViewController.h"
+
+@interface XCPageViewController ()
+
+@end
+
+@implementation XCPageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
