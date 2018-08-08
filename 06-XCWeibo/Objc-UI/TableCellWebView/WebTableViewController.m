ef809d037a7523c694886fec61ca0c6667694a32
Alexiuce <alexiuce@qq.com>
2018-08-08 13:32:23 +0800

    web tab update

--- 06-XCWeibo/Objc-UI/TableCellWebView/WebTableViewController.m
+++ 06-XCWeibo/Objc-UI/TableCellWebView/WebTableViewController.m
@@ -11,7 +11,9 @@
 #import "WebTableViewCell.h"
 
 
-@interface WebTableViewController ()
+@interface WebTableViewController ()<UIWebViewDelegate,UIScrollViewDelegate>
+
+@property (nonatomic, strong) UIWebView *webView;
 
 @end
 
@@ -19,17 +21,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.tableView.rowHeight = 200;
+    
+    self.tableView.tableHeaderView = self.webView;
+    
+    NSURL *url = [NSURL URLWithString:@"http://www.baidu.com"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:request];
+    self.webView.frame = CGRectMake(0, 0, 200, 200);
+    [self.tableView addSubview:self.webView];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 #pragma mark - Table view data source
-
-
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 
@@ -39,10 +43,41 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     WebTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"web_cell" forIndexPath:indexPath];
-    
 
-    
     return cell;
 }
 
+
+#pragma mark - scroll view delegate
+
+
+
+#pragma mark - kvo
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context{
+    if (![keyPath isEqualToString:@"contentSize"]) {return;}
+    CGSize webSize = [self.webView sizeThatFits:CGSizeZero];
+    NSLog(@"kvo content size = %@",NSStringFromCGSize(webSize));
+//    self.webView.frame = CGRectMake(0, 0, webSize.width, webSize.height);
+//    [self.tableView beginUpdates];
+//    [self.tableView setTableHeaderView:self.webView];
+//    [self.tableView endUpdates];
+    
+}
+
+#pragma mark - lazy getter
+
+
+- (UIWebView *)webView{
+    if (_webView == nil) {
+        _webView = [[UIWebView alloc]init];
+        [_webView.scrollView addObserver:self forKeyPath:@"contentSize" options:NSKeyValueObservingOptionNew context:nil];
+        
+    }
+    return _webView;
+}
+
+
+- (void)dealloc{
+    [_webView.scrollView removeObserver:self forKeyPath:@"contentSize"];
+}
 @end
