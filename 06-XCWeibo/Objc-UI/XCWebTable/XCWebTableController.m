cfc2eac8cc0d6a39699897ca451eee8c74d3cd4d
Alexiuce <alexiuce@qq.com>
2018-08-11 08:43:01 +0800

    XCWebTable setup ui

--- 06-XCWeibo/Objc-UI/XCWebTable/XCWebTableController.m
+++ 06-XCWeibo/Objc-UI/XCWebTable/XCWebTableController.m
@@ -7,8 +7,15 @@
 //
 
 #import "XCWebTableController.h"
+#import "UIView+ACMediaExt.h"
+#import <WebKit/WebKit.h>
 
-@interface XCWebTableController ()
+@interface XCWebTableController ()<UITableViewDataSource,UITableViewDelegate,UIScrollViewDelegate, WKNavigationDelegate>
+
+@property (nonatomic, strong) UIScrollView *scrollView;     // 主滚动视图
+@property (nonatomic, strong) UIView *scrollViewContainerView;    //  添加到scrollView中的容器视图
+@property (nonatomic, strong) UITableView *tableView;       // 添加到scrollViewContainerView中的列表视图
+@property (nonatomic, strong) WKWebView *webView;           // 添加到scrollViewContainerView中的web视图
 
 @end
 
@@ -16,8 +23,94 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    
+    [self.scrollViewContainerView addSubview:self.webView];
+    [self.scrollViewContainerView addSubview:self.tableView];
+    [self.scrollView addSubview:self.scrollViewContainerView];
+    [self.view addSubview:self.scrollView];
+    
+    NSURL *url = [NSURL URLWithString:@" https://www.jianshu.com/p/5ee6030aeb4f"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:request];
 }
 
+- (void)viewDidLayoutSubviews{
+    [super viewDidLayoutSubviews];
+    self.scrollViewContainerView.frame = CGRectMake(0, 0, self.view.width, self.view.height * 2);
+    
+}
+
+#pragma mark - table view data source
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 3;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"xc_table_cell"];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"xc_table_cell"];
+    }
+    cell.textLabel.text = @(indexPath.row).stringValue;
+    return cell;
+}
+
+#pragma mark - table view delegate
+
+#pragma mark - UIScrollView delegate
+
+
+
+
+#pragma mark - lazy getter method
+
+
+- (UIScrollView *)scrollView{
+    if (_scrollView == nil) {
+        _scrollView = [[UIScrollView alloc]initWithFrame:self.view.bounds];
+        _scrollView.delegate = self;
+        _scrollView.alwaysBounceVertical = YES;
+    }
+    return _scrollView;
+}
+
+- (UIView *)scrollViewContainerView{
+    if (_scrollViewContainerView == nil) {
+        _scrollViewContainerView = [[UIView alloc]init];
+    }
+    return _scrollViewContainerView;
+}
+
+- (WKWebView *)webView{
+    if (_webView == nil) {
+        WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc]init];
+        _webView = [[WKWebView alloc]initWithFrame:self.view.bounds configuration:config];
+        _webView.scrollView.scrollEnabled = NO;
+        _webView.navigationDelegate = self;
+    }
+    return _webView;
+}
+
+- (UITableView *)tableView{
+    if (_tableView == nil) {
+        _tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        _tableView.tableFooterView = [UIView new];
+        _tableView.scrollEnabled = NO;
+    }
+    return _tableView;
+}
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
