69135302a5fb1358b48ac082ca593a728a52d314
Alexiuce <alexiuce@qq.com>
2018-08-11 08:16:12 +0800

    XCWeibo add XCWebTable

---
+++ 06-XCWeibo/Objc-UI/XCWebTable/XCWebTableController.h
@@ -0,0 +1,13 @@
+//
+//  XCWebTableController.h
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/8/11.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XCWebTableController : UIViewController
+
+@end
---
+++ 06-XCWeibo/Objc-UI/XCWebTable/XCWebTableController.m
@@ -0,0 +1,23 @@
+//
+//  XCWebTableController.m
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/8/11.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+#import "XCWebTableController.h"
+
+@interface XCWebTableController ()
+
+@end
+
+@implementation XCWebTableController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+
+@end
