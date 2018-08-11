8d91eaa5c970301f182340b77f66928d08af479d
Alexiuce <alexiuce@qq.com>
2018-08-11 08:52:25 +0800

    XCWebTable add observer

--- 06-XCWeibo/Objc-UI/XCWebTable/XCWebTableController.m
+++ 06-XCWeibo/Objc-UI/XCWebTable/XCWebTableController.m
@@ -17,6 +17,9 @@
 @property (nonatomic, strong) UITableView *tableView;       // 添加到scrollViewContainerView中的列表视图
 @property (nonatomic, strong) WKWebView *webView;           // 添加到scrollViewContainerView中的web视图
 
+@property (nonatomic, assign) CGFloat maxWebContentHeight;
+@property (nonatomic, assign) CGFloat maxTabContentHeight;
+
 @end
 
 @implementation XCWebTableController
@@ -29,7 +32,7 @@
     [self.scrollView addSubview:self.scrollViewContainerView];
     [self.view addSubview:self.scrollView];
     
-    NSURL *url = [NSURL URLWithString:@" https://www.jianshu.com/p/5ee6030aeb4f"];
+    NSURL *url = [NSURL URLWithString:@"https://www.jianshu.com/p/5ee6030aeb4f"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
     [self.webView loadRequest:request];
 }
@@ -37,8 +40,21 @@
 - (void)viewDidLayoutSubviews{
     [super viewDidLayoutSubviews];
     self.scrollViewContainerView.frame = CGRectMake(0, 0, self.view.width, self.view.height * 2);
-    
+    self.tableView.y = CGRectGetMaxY(self.scrollView.bounds);
+    self.scrollView.contentSize = CGSizeMake(0, self.view.height * 2);
 }
+
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [self.tableView addObserver:self forKeyPath:@"contentSize" options:NSKeyValueObservingOptionNew context:nil];
+    [self.webView addObserver:self forKeyPath:@"scrollView.contentSize" options:NSKeyValueObservingOptionNew context:nil];
+}
+- (void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+    [self.tableView removeObserver:self forKeyPath:@"contentSize"];
+    [self.webView removeObserver:self forKeyPath:@"scrollView.contentSize"];
+}
+
 
 #pragma mark - table view data source
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
