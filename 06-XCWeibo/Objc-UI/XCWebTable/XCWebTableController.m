35a5fd98ce5d32643a7d728b1d9bc40a03122e94
Alexiuce <alexiuce@qq.com>
2018-08-11 10:02:19 +0800

    XCWebTable day up

--- 06-XCWeibo/Objc-UI/XCWebTable/XCWebTableController.m
+++ 06-XCWeibo/Objc-UI/XCWebTable/XCWebTableController.m
@@ -74,7 +74,7 @@
 
 #pragma mark - UIScrollView delegate
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView{
-    CGFloat offsetY = scrollView.contentOffset.y;
+    CGFloat offsetY = scrollView.contentOffset.y;  /* scrollview的 滑动偏移 */
 
     CGFloat webHeight = self.webView.height;
     CGFloat tabHeight = self.tableView.height;
@@ -82,27 +82,25 @@
     CGFloat webContentHeight = self.webView.scrollView.contentSize.height;
     CGFloat tabContentHeight = self.tableView.contentSize.height;
 
-    if (offsetY <= 0) {
+    if (offsetY <= 0) {   // 最顶部向上滑动
         self.scrollViewContainerView.y = 0;
         self.webView.scrollView.contentOffset = self.tableView.contentOffset = CGPointZero;
-    }else if (offsetY < webContentHeight - webHeight){
+    }else if (offsetY < webContentHeight - webHeight){  // web view 内容滚动区间
         self.scrollViewContainerView.y = offsetY;
         self.webView.scrollView.contentOffset = CGPointMake(0, offsetY);
         self.tableView.contentOffset = CGPointZero;;
-    }else if (offsetY < webContentHeight){
+    }else if (offsetY < webContentHeight){   // web view 已滚动最大范围
         self.scrollViewContainerView.y = webContentHeight - webHeight;
         self.webView.scrollView.contentOffset = CGPointMake(0, webContentHeight - webHeight);
         self.tableView.contentOffset = CGPointZero;
-    }else if (offsetY < webContentHeight + tabContentHeight - tabHeight){
+    }else if (offsetY < webContentHeight + tabContentHeight - tabHeight){ // table view 内容滚动区间
         self.scrollViewContainerView.y = offsetY - webHeight;
         self.webView.scrollView.contentOffset = CGPointMake(0, webContentHeight - webHeight);
         self.tableView.contentOffset = CGPointMake(0, offsetY - webContentHeight);
-    }else if (offsetY < webContentHeight + tabContentHeight){
+    }else if (offsetY < webContentHeight + tabContentHeight){  // table 已滚动到最大范围
         self.scrollViewContainerView.y = self.scrollView.contentSize.height - self.scrollViewContainerView.height;
         self.webView.scrollView.contentOffset = CGPointMake(0, webContentHeight - webHeight);
         self.tableView.contentOffset = CGPointMake(0, tabContentHeight - tabHeight);
-    }else{
-        NSLog(@"加载更多.....");
     }
 
 }
@@ -111,16 +109,20 @@
 #pragma mark - notification Handle
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context{
-    CGFloat webContentHeight = self.webView.scrollView.contentSize.height;
-    CGFloat tabContentHeight = self.tableView.contentSize.height;
+    
+    CGFloat webContentHeight = self.webView.scrollView.contentSize.height; /*web view 的内容高度 */
+    CGFloat tabContentHeight = self.tableView.contentSize.height;   /* tablie 的内容高度 */
     
     if (webContentHeight == _maxWebContentHeight && tabContentHeight == _maxTabContentHeight) {return;}
-    _maxWebContentHeight = webContentHeight;
-    _maxTabContentHeight = tabContentHeight;
+    _maxWebContentHeight = webContentHeight;  /* 保存最新web内容高度 */
+    _maxTabContentHeight = tabContentHeight;  /*  保存最新tab内容高度 */
+    // 设置总的视图滚动范围: web 内容高度 + tab 内容高度;
     self.scrollView.contentSize = CGSizeMake(self.view.width, webContentHeight + tabContentHeight);
+    
     CGFloat webHeight = MIN( webContentHeight, self.view.height)  ;
-    CGFloat tableHeight = MIN(tabContentHeight, self.view.height) ;
-    self.webView.height = MAX(0.1, webHeight);
+    CGFloat tableHeight = MIN(tabContentHeight, self.view.height) ;  // tab view 的最大高度不超过屏幕高度
+    self.webView.height = MAX(0.1, webHeight);    // 限制web view 的最大高度不超过屏幕高度
+    // 设置scrollview的高度 =  web view 的高度 + table view 的高度;
     self.scrollViewContainerView.height = webHeight + tableHeight;
     self.tableView.height = tableHeight;
     self.tableView.y = self.webView.bottom;
