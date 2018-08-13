67be5873158ab2da7e56aa06496a2b2b9e819815
Alexiuce <alexiuce@qq.com>
2018-08-13 08:25:55 +0800

    SDYStrategyBottomInputView

---
+++ 06-XCWeibo/Objc-UI/AnimationViewDemo/SDYStrategyBottomInputView.h
@@ -0,0 +1,13 @@
+//
+//  SDYStrategyBottomInputView.h
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/8/13.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDYStrategyBottomInputView : UIView
+
+@end
---
+++ 06-XCWeibo/Objc-UI/AnimationViewDemo/SDYStrategyBottomInputView.m
@@ -0,0 +1,21 @@
+//
+//  SDYStrategyBottomInputView.m
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/8/13.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+#import "SDYStrategyBottomInputView.h"
+
+@implementation SDYStrategyBottomInputView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
