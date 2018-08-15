2596ae3acbd9a6ea4695ff0f563dff49637dfbe4
Alexiuce <alexiuce@qq.com>
2018-08-15 09:06:59 +0800

    添加轮播页面

---
+++ 06-XCWeibo/Objc-UI/CycleScrollView/XCCycleController.h
@@ -0,0 +1,13 @@
+//
+//  XCCycleController.h
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/8/15.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XCCycleController : UIViewController
+
+@end
---
+++ 06-XCWeibo/Objc-UI/CycleScrollView/XCCycleController.m
@@ -0,0 +1,27 @@
+//
+//  XCCycleController.m
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/8/15.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+#import "XCCycleController.h"
+#import <SDCycleScrollView/SDCycleScrollView.h>
+
+@interface XCCycleController ()
+
+@end
+
+@implementation XCCycleController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    UIView *topView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 200)];
+    topView.backgroundColor = UIColor.lightGrayColor;
+    [self.view addSubview:topView];
+}
+
+
+
+@end
