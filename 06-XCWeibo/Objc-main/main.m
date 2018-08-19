692c32ab181d18736d1ffd3f647a8f1c412b4d24
Alexiuce <alexiuce@qq.com>
2018-08-19 08:50:58 +0800

    添加gcd code

--- 06-XCWeibo/Objc-main/main.m
+++ 06-XCWeibo/Objc-main/main.m
@@ -10,14 +10,17 @@
 
 
 
+int my_sum(int a,int b);
+void my_test(void);
 
-
+typedef void(^Tasklock)(void);
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        int result = my_sum(10, 20);
-        NSLog(@"%d",result);
+        my_test();
+        
+   
     }
     return 0;
 }
@@ -26,7 +29,35 @@ int my_sum(int a,int b){
     return  a + b;
 }
 
-void test1r(){
+void my_test(){
+    NSMutableArray *_mArray = [NSMutableArray array];
     
+    __block int operaterNumber = 0;
     
+    // 串行队列
+    dispatch_queue_t queue =  dispatch_queue_create("threand-one",DISPATCH_QUEUE_SERIAL);
+    
+    Tasklock task = ^{
+        if (operaterNumber % 2) {
+            NSLog(@"===%@",[NSThread currentThread]);
+            [_mArray addObject:@(operaterNumber)];
+            operaterNumber += 1;
+        }
+    };
+    Tasklock task1 = ^{
+        if (operaterNumber % 2 == 0) {
+            NSLog(@"===%@",[NSThread currentThread]);
+            [_mArray addObject:@(operaterNumber)];
+             operaterNumber += 1;
+        }
+    };
+    
+    // 队列中添加异步任务
+    
+    for (int i = 0; i < 10; i ++) {
+        Tasklock execTask = i % 2 ? task : task1;
+        dispatch_async(queue, execTask);
+    }
+    NSLog(@"----------");
+    NSLog(@"%@",_mArray);
 }
