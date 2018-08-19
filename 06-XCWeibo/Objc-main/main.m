fe0da4776f4a4090f52c93295884658c3af5e19b
Alexiuce <alexiuce@qq.com>
2018-08-19 08:07:44 +0800

    添加Objc-main target

---
+++ 06-XCWeibo/Objc-main/main.m
@@ -0,0 +1,32 @@
+//
+//  main.m
+//  Objc-main
+//
+//  Created by Alexcai on 2018/8/19.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+
+
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        int result = my_sum(10, 20);
+        NSLog(@"%d",result);
+    }
+    return 0;
+}
+
+int my_sum(int a,int b){
+    return  a + b;
+}
+
+void test1r(){
+    
+    
+}
