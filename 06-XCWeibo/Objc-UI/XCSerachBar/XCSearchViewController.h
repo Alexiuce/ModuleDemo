1ba242544664babf17f6fa899bd733f6cc644d8b
Alexiuce <alexiuce@qq.com>
2018-08-20 08:06:56 +0800

    添加Search Bar Storyboard

---
+++ 06-XCWeibo/Objc-UI/XCSerachBar/XCSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  XCSearchViewController.h
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/8/20.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XCSearchViewController : UIViewController
+
+@end
---
+++ 06-XCWeibo/Objc-UI/XCSerachBar/XCSearchViewController.m
@@ -0,0 +1,37 @@
+//
+//  XCSearchViewController.m
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/8/20.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+#import "XCSearchViewController.h"
+
+@interface XCSearchViewController ()
+
+@end
+
+@implementation XCSearchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
