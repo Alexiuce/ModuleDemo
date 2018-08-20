1bcd00d307af2801d840562860b1df38960c753e
Alexiuce <alexiuce@qq.com>
2018-08-20 08:26:17 +0800

    设置serach icon

--- 06-XCWeibo/Objc-UI/XCSerachBar/XCSearchViewController.m
+++ 06-XCWeibo/Objc-UI/XCSerachBar/XCSearchViewController.m
@@ -10,28 +10,20 @@
 
 @interface XCSearchViewController ()
 
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+
 @end
 
 @implementation XCSearchViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self.searchBar setImage:[UIImage imageNamed:@"icon_search1"] forSearchBarIcon:UISearchBarIconSearch state:UIControlStateNormal];
+    
+    
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
