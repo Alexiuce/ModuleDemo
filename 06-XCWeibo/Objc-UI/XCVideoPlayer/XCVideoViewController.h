7048192373e18d809917809a54105c8ac6b752fd
Alexiuce <alexiuce@qq.com>
2018-08-23 08:05:44 +0800

    添加video Storyboard

---
+++ 06-XCWeibo/Objc-UI/XCVideoPlayer/XCVideoViewController.h
@@ -0,0 +1,13 @@
+//
+//  XCVideoViewController.h
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/8/23.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XCVideoViewController : UIViewController
+
+@end
