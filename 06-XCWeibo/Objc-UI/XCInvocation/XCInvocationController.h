7af328779fe40c859c3531fc88d49c19cafe4886
Alexiuce <alexiuce@qq.com>
2018-08-29 16:42:49 +0800

    添加XCInvocation

---
+++ 06-XCWeibo/Objc-UI/XCInvocation/XCInvocationController.h
@@ -0,0 +1,13 @@
+//
+//  XCInvocationController.h
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/8/29.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XCInvocationController : UIViewController
+
+@end
