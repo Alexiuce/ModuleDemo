ce8625f73250bba19d4f3c6592b50e00a5436684
Alexiuce <alexiuce@qq.com>
2018-09-06 08:52:41 +0800

    test(UIImageView): 显示模式
    尝试不同的contentViewMode 值效果

--- 06-XCWeibo/Objc-UI/ScrollViewAutolayout/SALViewController.m
+++ 06-XCWeibo/Objc-UI/ScrollViewAutolayout/SALViewController.m
@@ -10,6 +10,7 @@
 #import <SDWebImage/UIImageView+WebCache.h>
 
 @interface SALViewController ()
+
 @property (weak, nonatomic) IBOutlet UIImageView *pictureView;
 
 @end
@@ -19,7 +20,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    NSString *imageUrl = @"http://t2.hddhhn.com/uploads/tu/201610/198/scx30045vxd.jpg";
+    
+    NSString *imageUrl = @"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1536204835442&di=6daf59e61f13cb3ee2fed1f9b615dc6f&imgtype=0&src=http%3A%2F%2Fp3.wmpic.me%2Farticle%2F2016%2F08%2F31%2F1472612447_LlwuSeKC.jpg";
+    //@"http://t2.hddhhn.com/uploads/tu/201610/198/scx30045vxd.jpg";
     [_pictureView sd_setImageWithURL:[NSURL URLWithString:imageUrl]];
 
 }
