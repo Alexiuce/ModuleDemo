d3b8d1bde6d9108c5c9e084e8977d93e79d327ff
Alexiuce <alexiuce@qq.com>
2018-09-07 08:50:32 +0800

    test(WKWebview): 自动布局
    WKWebview 没有添加宽高约束时,无法显示加载的url内容

--- 06-XCWeibo/Objc-UI/StackView/StackViewController.m
+++ 06-XCWeibo/Objc-UI/StackView/StackViewController.m
@@ -8,6 +8,7 @@
 
 #import "StackViewController.h"
 #import <SDWebImage/UIImageView+WebCache.h>
+#import <WebKit/WebKit.h>
 
 
 @interface StackViewController ()
@@ -20,6 +21,12 @@
 @property (weak, nonatomic) IBOutlet UILabel *nameLabel;
 
 
+@property (weak, nonatomic) IBOutlet UIStackView *leftStackView;
+
+@property (weak, nonatomic) IBOutlet WKWebView *webView;
+
+
+@property (weak, nonatomic) IBOutlet WKWebView *bottomWebView;
 
 @end
 
@@ -32,6 +39,12 @@
     [_topPicture sd_setImageWithURL:[NSURL URLWithString:imageUrl]];
     
     [_iconPictureView sd_setImageWithURL:[NSURL URLWithString:iconUrl]];
+    
+    _nameLabel.text = @"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1536204835442&di=6daf59e61f13cb3ee2fed1f9b615dc6f&imgtype=0&src=http%3A%2F%2Fp3.wmpic.me%2Farticle%2F2016%2F08%2F31%2F1472612447_LlwuSeKC.jpg";
+    
+    [_webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://www.jianshu.com/p/b6b30d38255e"]]];
+    
+     [_bottomWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://www.jianshu.com/p/b6b30d38255e"]]];
 
 }
 
