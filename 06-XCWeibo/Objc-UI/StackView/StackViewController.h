707dc880d4e02eb0f2b11e4eaad6a5bd704e9a17
Alexiuce <alexiuce@qq.com>
2018-09-07 08:06:55 +0800

    feat(StackView布局): demo
    添加一个stackView ,并使用自动布局样例

---
+++ 06-XCWeibo/Objc-UI/StackView/StackViewController.h
@@ -0,0 +1,13 @@
+//
+//  StackViewController.h
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/9/7.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StackViewController : UIViewController
+
+@end
---
+++ 06-XCWeibo/Objc-UI/StackView/StackViewController.m
@@ -0,0 +1,37 @@
+//
+//  StackViewController.m
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/9/7.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+#import "StackViewController.h"
+
+@interface StackViewController ()
+
+@end
+
+@implementation StackViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
