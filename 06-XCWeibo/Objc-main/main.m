f1bf7b40e376cb00fd354fe3dd01f76e114df6a6
Alexiuce <alexiuce@qq.com>
2018-09-12 08:20:33 +0800

    refactor(文件读取): 使用NSFileHandle 读取文件内容
    获取指定文件后,读取文件指定长度的一段内容

--- 06-XCWeibo/Objc-main/main.m
+++ 06-XCWeibo/Objc-main/main.m
@@ -21,24 +21,24 @@ int main(int argc, const char * argv[]) {
         
         XCFileManager *m = [XCFileManager shareManager];
         NSArray * result = [m listAllFilesInPath:@"/Users/Alexcai/SDY/newSDYiOS" fileType:DOT_STAR];
-
-        NSLog(@"%@",result);
 //
-        [result enumerateObjectsUsingBlock:^(NSString *  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-            if ([obj hasSuffix:@"\""]){
-                NSLog(@"====%@",obj);
-            }
-        }];
-       
-//        NSFileHandle *fileHandle = [NSFileHandle fileHandleForReadingAtPath:@"/Users/Alexcai/Desktop/XCFileManager.m"];
-//        // 读取完成执行block
-//        [fileHandle setReadabilityHandler:^(NSFileHandle * _Nonnull handle) {
-//            NSLog(@"----handle == %@",handle);
+//        NSLog(@"%@",result);
+////
+//        [result enumerateObjectsUsingBlock:^(NSString *  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+//            if ([obj hasSuffix:@"\""]){
+//                NSLog(@"====%@",obj);
+//            }
 //        }];
-//        NSData *readData = [fileHandle readDataOfLength:50];
-//
-//        NSString *readText = [[NSString alloc]initWithData:readData encoding:NSUTF8StringEncoding];
-//        NSLog(@"%@",readText);
+       
+        NSFileHandle *fileHandle = [NSFileHandle fileHandleForReadingAtPath:@"/Users/Alexcai/Desktop/XCFileManager.m"];
+        // 读取完成执行block
+        [fileHandle setReadabilityHandler:^(NSFileHandle * _Nonnull handle) {
+            NSLog(@"----handle == %@",handle);
+        }];
+        NSData *readData = [fileHandle readDataOfLength:50];
+
+        NSString *readText = [[NSString alloc]initWithData:readData encoding:NSUTF8StringEncoding];
+        NSLog(@"%@",readText);
         
         
         
