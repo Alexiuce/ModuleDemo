c74ea726465ee7d1bdd3220e4d6296817704a432
Alexiuce <alexiuce@qq.com>
2018-09-19 08:38:26 +0800

    refactor(UILabel Autolayout): 添加隐藏与消失的动画过渡效果
    使用alpha替代hiden 实现过渡动画(消失/隐藏)效果

--- 06-XCWeibo/Objc-UI/LabelConstraint/XCLabelConstraintViewController.m
+++ 06-XCWeibo/Objc-UI/LabelConstraint/XCLabelConstraintViewController.m
@@ -10,6 +10,10 @@
 
 @interface XCLabelConstraintViewController ()
 
+@property (weak, nonatomic) IBOutlet UIView *containerView;
+@property (weak, nonatomic) IBOutlet UILabel *l1;
+@property (weak, nonatomic) IBOutlet UILabel *l2;
+@property (weak, nonatomic) IBOutlet UILabel *l3;
 
 @property (strong, nonatomic) IBOutlet NSLayoutConstraint *label3ToSuperLeftConstraint;
 
@@ -26,23 +30,26 @@
 
 
 - (IBAction)clickLeft:(UIButton *)sender {
+    
     [self.label3LeftMarginConstraint setActive:NO];
     
-    [UIView animateWithDuration:2 animations:^{
+    [UIView animateWithDuration:0.35 animations:^{
+        self.l1.alpha = self.l2.alpha = 0;
         self.label3ToSuperLeftConstraint.constant = 16.0f;
         [self.label3ToSuperLeftConstraint setActive:YES];
-        [self.view layoutIfNeeded];
+        [self.containerView layoutIfNeeded];
         
     }];
     
 }
 - (IBAction)clickReset:(UIButton *)sender {
     [self.label3ToSuperLeftConstraint setActive:NO];
-    [UIView animateWithDuration:2 animations:^{
+    [UIView animateWithDuration:0.35 animations:^{
       
+        self.l1.alpha = self.l2.alpha = 1;
         [self.label3LeftMarginConstraint setActive:YES];
        
-        [self.view layoutIfNeeded];
+        [self.containerView layoutIfNeeded];
         
     }];
 
