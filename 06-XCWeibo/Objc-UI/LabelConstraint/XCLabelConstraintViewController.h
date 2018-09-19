f987eb9c8329811ab243ca7ea5d2f82312cddd53
Alexiuce <alexiuce@qq.com>
2018-09-19 08:11:12 +0800

    feat(Label 约束优先级): 初始化UI
    添加三个label,并设置相对约束位置

---
+++ 06-XCWeibo/Objc-UI/LabelConstraint/XCLabelConstraintViewController.h
@@ -0,0 +1,13 @@
+//
+//  XCLabelConstraintViewController.h
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/9/19.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XCLabelConstraintViewController : UIViewController
+
+@end
---
+++ 06-XCWeibo/Objc-UI/LabelConstraint/XCLabelConstraintViewController.m
@@ -0,0 +1,37 @@
+//
+//  XCLabelConstraintViewController.m
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/9/19.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+#import "XCLabelConstraintViewController.h"
+
+@interface XCLabelConstraintViewController ()
+
+@end
+
+@implementation XCLabelConstraintViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
