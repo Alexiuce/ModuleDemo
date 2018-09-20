d396a86a5bc3ede148defd4c4472f82f431c0aec
Alexiuce <alexiuce@qq.com>
2018-09-20 08:21:10 +0800

    feat(UILayoutGuid): 使用布局介质对象代替占位View
    进行布局时,使用UILayoutGuide 替代占位View,减少渲染和响应链

--- 06-XCWeibo/Objc-UI/LabelConstraint/XCLabelConstraintViewController.m
+++ 06-XCWeibo/Objc-UI/LabelConstraint/XCLabelConstraintViewController.m
@@ -19,6 +19,13 @@
 
 @property (strong, nonatomic) IBOutlet NSLayoutConstraint *label3LeftMarginConstraint;
 
+
+@property (nonatomic, strong)UILayoutGuide *layoutGuide;
+
+@property (weak, nonatomic) IBOutlet UILabel *l4;
+@property (weak, nonatomic) IBOutlet UILabel *l5;
+
+
 @end
 
 @implementation XCLabelConstraintViewController
@@ -26,6 +33,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    
+    self.layoutGuide = [UILayoutGuide new];
+    [self.view addLayoutGuide:self.layoutGuide];
+    
+    [self.layoutGuide.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor].active = YES;
+    [self.layoutGuide.leftAnchor constraintEqualToAnchor:self.view.leftAnchor].active = YES;
+    [self.layoutGuide.rightAnchor constraintEqualToAnchor:self.view.rightAnchor].active = YES;
+    [self.layoutGuide.heightAnchor constraintEqualToConstant:100].active = YES;
+    
+    self.l4.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    
+    
+    [self.l4.topAnchor constraintEqualToAnchor:self.layoutGuide.topAnchor constant:20].active = YES;
+    
+    [self.l4.leftAnchor constraintEqualToAnchor:self.layoutGuide.leftAnchor].active = YES;
+    [self.l5.bottomAnchor constraintEqualToAnchor:self.layoutGuide.bottomAnchor].active = YES;
+    [self.l5.rightAnchor constraintEqualToAnchor:self.layoutGuide.rightAnchor].active = YES;
+    
+    
+    
+    
 }
 
 
