0cdcee3fc8ac9cb017045a279ecc2aa2fa5530e2
Alexiuce <alexiuce@qq.com>
2018-09-21 08:19:19 +0800

    feat(XCSubViewFrame): 从Xib中加载view ,并获取view中子控件的frame值

---
+++ 06-XCWeibo/Objc-UI/LabelConstraint/Views/XCSubViewFrameView.h
@@ -0,0 +1,13 @@
+//
+//  XCSubViewFrameView.h
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/9/21.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XCSubViewFrameView : UIView
+
+@end
---
+++ 06-XCWeibo/Objc-UI/LabelConstraint/Views/XCSubViewFrameView.m
@@ -0,0 +1,21 @@
+//
+//  XCSubViewFrameView.m
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/9/21.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+#import "XCSubViewFrameView.h"
+
+@implementation XCSubViewFrameView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
