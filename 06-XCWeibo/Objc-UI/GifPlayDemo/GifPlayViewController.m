2d1cfb61f39dcdbefb88f586112f8e70cdd19e3f
Alexiuce <alexiuce@qq.com>
2018-09-22 15:45:47 +0800

    refactor(GifPlay): 添加core foundation 内存管理代码

--- 06-XCWeibo/Objc-UI/GifPlayDemo/GifPlayViewController.m
+++ 06-XCWeibo/Objc-UI/GifPlayDemo/GifPlayViewController.m
@@ -27,10 +27,34 @@
     CGImageSourceRef imageSourceRef = CGImageSourceCreateWithData((__bridge CFDataRef)gifData, NULL);
     // 3. 获取gif 的图片总数;
     int imgCount = (int)CGImageSourceGetCount(imageSourceRef);
+    NSMutableArray <UIImage *>*allImageArray = [NSMutableArray arrayWithCapacity:imgCount];
+    NSTimeInterval allPlayDuration = 0;
     // 4. 根据索引获取指定位置的图片;
-    CGImageRef imgRef = CGImageSourceCreateImageAtIndex(imageSourceRef, imgCount - 1, NULL);
-    // 5. 显示图片;
-    self.imgView.image = [UIImage imageWithCGImage:imgRef];
+    for (int i = 0; i < imgCount; ++i) {
+        // 4.0 获取图片引用;
+        CGImageRef imgRef = CGImageSourceCreateImageAtIndex(imageSourceRef, i, NULL);
+        UIImage *img = [UIImage imageWithCGImage:imgRef];
+        [allImageArray addObject:img];
+        // 释放cf 对象
+        CFRelease(imgRef);
+        // 4.1 获取gif图片相关属性;
+        CFDictionaryRef dictRef = CGImageSourceCopyPropertiesAtIndex(imageSourceRef, i, NULL);
+        // 4.2 获取gif 属性字典
+        CFDictionaryRef gifDictRef = CFDictionaryGetValue(dictRef, kCGImagePropertyGIFDictionary);
+        // 4.3 获取一帧的播放时长
+        NSNumber *frameDuration = CFDictionaryGetValue(gifDictRef, kCGImagePropertyGIFDelayTime);
+        allPlayDuration += frameDuration.doubleValue;
+        // 释放cf 对象
+        CFRelease(dictRef);
+        
+    }
+    // 释放core foundation 对象
+    CFRelease(imageSourceRef);
+    // 5. 显示动画图片;
+    self.imgView.animationImages = allImageArray;
+    self.imgView.animationDuration = allPlayDuration;
+    [self.imgView startAnimating];
+    
     
 
 }
