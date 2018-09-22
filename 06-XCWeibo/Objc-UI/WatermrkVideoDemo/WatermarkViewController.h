714f8a333ba496cf6b4e2b99f7a6d41816208b05
Alexiuce <alexiuce@qq.com>
2018-09-22 13:40:16 +0800

    feat(Watermark for video): 添加水印示例Storyboard
    测试视频添加水印效果示例代码

---
+++ 06-XCWeibo/Objc-UI/WatermrkVideoDemo/WatermarkViewController.h
@@ -0,0 +1,13 @@
+//
+//  WatermarkViewController.h
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/9/22.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WatermarkViewController : UIViewController
+
+@end
