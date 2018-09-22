887c1818f0fc4682ed203a9d2b802317ba4d4a98
Alexiuce <alexiuce@qq.com>
2018-09-22 14:21:22 +0800

    feat(Gif): 添加Gif 播放示例
    控制git的播放次数

---
+++ 06-XCWeibo/Objc-UI/GifPlayDemo/GifPlayViewController.h
@@ -0,0 +1,13 @@
+//
+//  GifPlayViewController.h
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/9/22.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GifPlayViewController : UIViewController
+
+@end
