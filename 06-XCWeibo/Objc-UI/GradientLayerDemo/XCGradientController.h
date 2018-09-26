11a388e24c9086d2eb75ca7c733521c1239e7a78
Alexiuce <alexiuce@qq.com>
2018-09-26 08:39:08 +0800

    feat(GradientDemo): 颜色渐变
    使用CAGradientLayer 实现颜色渐变

---
+++ 06-XCWeibo/Objc-UI/GradientLayerDemo/XCGradientController.h
@@ -0,0 +1,13 @@
+//
+//  XCGradientController.h
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/9/26.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XCGradientController : UIViewController
+
+@end
---
+++ 06-XCWeibo/Objc-UI/GradientLayerDemo/XCGradientController.m
@@ -0,0 +1,18 @@
+//
+//  XCGradientController.m
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/9/26.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+#import "XCGradientController.h"
+
+@implementation XCGradientController
+
+- (void)viewDidLoad{
+    [super viewDidLoad];
+    
+}
+
+@end
