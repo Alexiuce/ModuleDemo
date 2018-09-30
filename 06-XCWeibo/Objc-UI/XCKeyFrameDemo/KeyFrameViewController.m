f161454f6688a496adbefc4f697be9236395c798
Alexiuce <alexiuce@qq.com>
2018-09-30 09:11:00 +0800

    refactor(CALayer key frame): 帧动画 修正

--- 06-XCWeibo/Objc-UI/XCKeyFrameDemo/KeyFrameViewController.m
+++ 06-XCWeibo/Objc-UI/XCKeyFrameDemo/KeyFrameViewController.m
@@ -25,7 +25,7 @@
     layer.bounds = (CGRect){{0,0},{48,32}};
     layer.backgroundColor = UIColor.redColor.CGColor;
     layer.anchorPoint = CGPointZero;
-    
+    layer.position = CGPointMake(-48, 0);
     [self.containerView.layer addSublayer:layer];
     _animateLayer = layer;
 
@@ -41,7 +41,7 @@
 //    CAAnimationGroup *animGroup = [CAAnimationGroup animation];
     
     
-    CABasicAnimation *anim1 = [CABasicAnimation animationWithKeyPath:@"position"];
+    CABasicAnimation *anim1 = [CABasicAnimation animationWithKeyPath:@"position.x"];
     
     anim1.fromValue = @(-48);
     anim1.toValue = @(80);
@@ -60,7 +60,8 @@
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    [self baseAnimate];
+//    [self baseAnimate];
+    [self keyframeAnimate];
 }
 
 - (void)keyframeAnimate{
@@ -68,14 +69,14 @@
     
     NSValue *v1 =  [NSValue valueWithCGPoint:CGPointMake(-48, 0)];
     NSValue *v2 = [NSValue valueWithCGPoint:CGPointMake(80, 0)];
-    NSValue *v3 = [NSValue valueWithCGPoint:CGPointMake(81, 0)];
-    NSValue *v4 = [NSValue valueWithCGPoint:CGPointMake(-48, 0)];
+    NSValue *v3 = [NSValue valueWithCGPoint:CGPointMake(80.1, 0)];
+
     
     
-    keyAnimation.values = @[v1,v2,v3,v4];
-    keyAnimation.duration = 2.6;
-    keyAnimation.keyTimes = @[@0,@0.12,@0.76,@0.12];
-    keyAnimation.repeatCount = MAXFLOAT;
+    keyAnimation.values = @[v1,v2,v3];
+    keyAnimation.duration = 2.3;
+    keyAnimation.keyTimes = @[@0,@0.13,@0.87];
+//    keyAnimation.repeatCount = MAXFLOAT;
     keyAnimation.autoreverses = YES;
     
     [self.animateLayer addAnimation:keyAnimation forKey:nil];
