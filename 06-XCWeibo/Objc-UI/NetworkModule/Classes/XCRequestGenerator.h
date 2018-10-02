9f633f1359c6c93c07966af09cc5d35472b3bac5
Alexiuce <alexiuce@qq.com>
2018-10-02 14:43:20 +0800

    refactor(Network moduel): 网络请求
    创建请求生成类

---
+++ 06-XCWeibo/Objc-UI/NetworkModule/Classes/XCRequestGenerator.h
@@ -0,0 +1,21 @@
+//
+//  XCRequestGenerator.h
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/10/2.
+//  Copyright © 2018 Alexcai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class RequestModel;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCRequestGenerator : NSObject
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 06-XCWeibo/Objc-UI/NetworkModule/Classes/XCRequestGenerator.m
@@ -0,0 +1,22 @@
+//
+//  XCRequestGenerator.m
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/10/2.
+//  Copyright © 2018 Alexcai. All rights reserved.
+//  生成NSURLRequest 请求
+
+#import "XCRequestGenerator.h"
+#import "RequestModel.h"
+
+@implementation XCRequestGenerator
+
+
+
+- (NSURL *)generateRequest:(RequestModel *)model{
+    
+    
+    return nil;
+}
+
+@end
