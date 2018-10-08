10bf52e0e97601610a12895fdbd98d663b23308e
Alexiuce <alexiuce@qq.com>
2018-10-08 08:44:41 +0800

    refactor(XCNetworkModule):
    添加Http业务控制器

---
+++ 06-XCWeibo/Objc-UI/XCNetworkModule/HttpRequestViewController.h
@@ -0,0 +1,17 @@
+//
+//  HttpRequestViewController.h
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/10/8.
+//  Copyright © 2018 Alexcai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HttpRequestViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 06-XCWeibo/Objc-UI/XCNetworkModule/HttpRequestViewController.m
@@ -0,0 +1,32 @@
+//
+//  HttpRequestViewController.m
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/10/8.
+//  Copyright © 2018 Alexcai. All rights reserved.
+//
+
+#import "HttpRequestViewController.h"
+
+@interface HttpRequestViewController ()
+
+@end
+
+@implementation HttpRequestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
