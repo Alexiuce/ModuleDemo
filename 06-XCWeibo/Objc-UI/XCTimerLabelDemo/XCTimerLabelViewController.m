077f94394fd0121461d6549a970f1ae7e922b971
Alexiuce <alexiuce@qq.com>
2018-10-09 09:41:19 +0800

    refactor(XCTimerLabel): 解决GCD Timer 循环引用
    使用weakSelf 代替self ,,防止GCD Timer 循环引用导致内存泄漏

--- 06-XCWeibo/Objc-UI/XCTimerLabelDemo/XCTimerLabelViewController.m
+++ 06-XCWeibo/Objc-UI/XCTimerLabelDemo/XCTimerLabelViewController.m
@@ -18,6 +18,8 @@
 
 @property (nonatomic, strong) dispatch_source_t gcd_timer;
 
+@property (nonatomic, strong) dispatch_source_t gcdTimer;
+
 @end
 
 @implementation XCTimerLabelViewController
@@ -50,15 +52,34 @@
         NSLog(@"gcd timer thread %@",NSThread.currentThread);
          weakSelf.timerCount++;
         dispatch_async(dispatch_get_main_queue(), ^{
-            weakSelf.textLayer.string = [NSString stringWithFormat:@"%zd",self.timerCount];
+            weakSelf.textLayer.string = [NSString stringWithFormat:@"%zd",weakSelf.timerCount];
         });
     });
     dispatch_resume(_gcd_timer);
-}
-
-- (void)viewDidDisappear:(BOOL)animated{
-    [super viewDidDisappear:animated];
-    dispatch_cancel(self.gcd_timer);
+    
+    //1.队列
+//    dispatch_queue_t queue = dispatch_get_global_queue(0, 0);
+//    //2.创建定时器
+//    self.gcdTimer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
+//    //3.设置时间
+//    // start 秒后开始执行
+//    uint64_t start = 2.0;
+//    // 每隔interval执行
+//    uint64_t interval = 1.0;
+//
+//    dispatch_source_set_timer(self.gcdTimer, dispatch_time(DISPATCH_TIME_NOW, start * NSEC_PER_SEC), interval * NSEC_PER_SEC, 0);
+//
+//    //4.设置回调
+//    __weak typeof(self)weakSelf = self;
+//    dispatch_source_set_event_handler(self.gcdTimer, ^{
+//        weakSelf.timerCount++;
+//        dispatch_async(dispatch_get_main_queue(), ^{
+//            [weakSelf countDownTimer];
+//        });
+//    });
+//    //5.启动定时器
+//    dispatch_resume(self.gcdTimer);
+    
 }
 
 
