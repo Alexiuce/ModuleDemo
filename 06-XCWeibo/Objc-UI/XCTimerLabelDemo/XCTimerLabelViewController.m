ac255d7716db8966f71d137c155c987ddd1d5752
Alexiuce <alexiuce@qq.com>
2018-10-09 08:31:06 +0800

    refactor(XCTimerLabel): NSTimer 循环引用
    NSTimer设置repeat 为yes会强引用控制器,造成内存泄漏

--- 06-XCWeibo/Objc-UI/XCTimerLabelDemo/XCTimerLabelViewController.m
+++ 06-XCWeibo/Objc-UI/XCTimerLabelDemo/XCTimerLabelViewController.m
@@ -11,23 +11,26 @@
 @interface XCTimerLabelViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *timerLabel;
 
+@property (nonatomic, strong) NSTimer *timer;  // 定时器
+
 @end
 
 @implementation XCTimerLabelViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    _timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(countDownTimer) userInfo:nil repeats:YES];
+    
     // Do any additional setup after loading the view.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)countDownTimer{
+    NSLog(@"111111");
 }
-*/
+
+- (void)dealloc{
+    NSLog(@"%s",__FUNCTION__);
+}
 
 @end
