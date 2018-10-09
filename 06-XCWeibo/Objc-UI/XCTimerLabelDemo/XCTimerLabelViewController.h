2276a8f8439158ad0cfb8ddac4f875ff4e241ba9
Alexiuce <alexiuce@qq.com>
2018-10-09 08:08:43 +0800

    feat(XCTimerLabel): 添加后台允许timer的示例

---
+++ 06-XCWeibo/Objc-UI/XCTimerLabelDemo/XCTimerLabelViewController.h
@@ -0,0 +1,17 @@
+//
+//  XCTimerLabelViewController.h
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/10/9.
+//  Copyright © 2018 Alexcai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCTimerLabelViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
