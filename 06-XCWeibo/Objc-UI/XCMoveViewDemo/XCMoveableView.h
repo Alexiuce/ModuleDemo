5d848c8bc41f140b1b65fbf2ee5d4abbae38e61f
Alexiuce <alexiuce@qq.com>
2018-10-11 08:42:51 +0800

    feat(XCMoveableView): 添加可以移动拖拽的view (初始化)

---
+++ 06-XCWeibo/Objc-UI/XCMoveViewDemo/XCMoveableView.h
@@ -0,0 +1,17 @@
+//
+//  XCMoveableView.h
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/10/11.
+//  Copyright © 2018 Alexcai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCMoveableView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 06-XCWeibo/Objc-UI/XCMoveViewDemo/XCMoveableView.m
@@ -0,0 +1,17 @@
+//
+//  XCMoveableView.m
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/10/11.
+//  Copyright © 2018 Alexcai. All rights reserved.
+//
+
+#import "XCMoveableView.h"
+
+@implementation XCMoveableView
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
+    NSLog(@"adsfasdf");
+}
+
+@end
