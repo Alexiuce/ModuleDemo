5f34afb8d7a157f5cb69c5669426afc31857203f
Alexiuce <alexiuce@qq.com>
2018-10-11 17:04:11 +0800

    feat(XCPushTransition): 添加push 转场动画Demo

---
+++ 06-XCWeibo/Objc-UI/XCNavigationPushAnimationDemo/XCPushAnimationViewController.m
@@ -0,0 +1,32 @@
+//
+//  XCPushAnimationViewController.m
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/10/11.
+//  Copyright © 2018 Alexcai. All rights reserved.
+//
+
+#import "XCPushAnimationViewController.h"
+
+@interface XCPushAnimationViewController ()
+
+@end
+
+@implementation XCPushAnimationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ 06-XCWeibo/Objc-UI/XCNavigationPushAnimationDemo/XCTransitionPushManager.h
@@ -0,0 +1,18 @@
+//
+//  XCTransitionPushManager.h
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/10/11.
+//  Copyright © 2018 Alexcai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCTransitionPushManager : NSObject<UIViewControllerAnimatedTransitioning>
+
+@end
+
+NS_ASSUME_NONNULL_END
