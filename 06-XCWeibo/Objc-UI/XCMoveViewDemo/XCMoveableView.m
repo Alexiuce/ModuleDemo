7bf186dbcbb5ba11f14dd7abd662b002b9fd1bbf
Alexiuce <alexiuce@qq.com>
2018-10-11 08:49:53 +0800

    refactor(XCMoveableView): 添加拖拽代码

--- 06-XCWeibo/Objc-UI/XCMoveViewDemo/XCMoveableView.m
+++ 06-XCWeibo/Objc-UI/XCMoveViewDemo/XCMoveableView.m
@@ -11,7 +11,20 @@
 @implementation XCMoveableView
 
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
-    NSLog(@"adsfasdf");
+    //做UIView拖拽
+    UITouch *touch = [touches anyObject];
+    
+    
+    //求偏移量 = 手指当前点的X - 手指上一个点的X
+    CGPoint curP = [touch locationInView:self];
+    CGPoint preP = [touch previousLocationInView:self];
+
+    
+    CGFloat offsetX = curP.x - preP.x;
+    CGFloat offsetY = curP.y - preP.y;
+    
+    //平移
+    self.transform = CGAffineTransformTranslate(self.transform, offsetX, offsetY);
 }
 
 @end
