26b608d015aab2fa1e498b7f52f02cf83e80c8c7
Alexiuce <alexiuce@qq.com>
2018-10-13 11:04:04 +0800

    feat(XCURLProtocolDemo): 添加NSURLProtocol 示例 ,实现应用内抓包

---
+++ 06-XCWeibo/Objc-UI/XCURLProtocolDemo/URLProtocolViewController.h
@@ -0,0 +1,17 @@
+//
+//  URLProtocolViewController.h
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/10/13.
+//  Copyright © 2018 Alexcai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface URLProtocolViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 06-XCWeibo/Objc-UI/XCURLProtocolDemo/URLProtocolViewController.m
@@ -0,0 +1,32 @@
+//
+//  URLProtocolViewController.m
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/10/13.
+//  Copyright © 2018 Alexcai. All rights reserved.
+//
+
+#import "URLProtocolViewController.h"
+
+@interface URLProtocolViewController ()
+
+@end
+
+@implementation URLProtocolViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
