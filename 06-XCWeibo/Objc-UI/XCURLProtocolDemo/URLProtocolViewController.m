f808983661894e37a77858828eade4ec6a168ef5
Alexiuce <alexiuce@qq.com>
2018-10-14 21:21:46 +0800

    refactor(XCURLProtocolDemo): 添加URLSession 发送网络请求

--- 06-XCWeibo/Objc-UI/XCURLProtocolDemo/URLProtocolViewController.m
+++ 06-XCWeibo/Objc-UI/XCURLProtocolDemo/URLProtocolViewController.m
@@ -16,13 +16,27 @@
 @implementation URLProtocolViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
 }
 #pragma mark - IBAction Event Handler
 /** 点击网络请求按钮事件处理 */
 - (IBAction)clickRequestButtonEvent:(UIButton *)sender {
 
+    
+    NSURL *url = [NSURL URLWithString:@"http://www.baidu.com"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    NSURLSession *session = [[NSURLSession alloc]init];
+   
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        NSString *result = [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"%@",result);
+        
+    }];
+    [dataTask resume];
 
 }
 
