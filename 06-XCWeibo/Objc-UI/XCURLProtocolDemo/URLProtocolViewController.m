b556b6a6a2de6daf04a8f453c56e323dbf64b082
Alexiuce <alexiuce@qq.com>
2018-10-14 21:38:31 +0800

    refactor(XCURLProtocolDemo): 添加URLSession 网络请求代码修正

--- 06-XCWeibo/Objc-UI/XCURLProtocolDemo/URLProtocolViewController.m
+++ 06-XCWeibo/Objc-UI/XCURLProtocolDemo/URLProtocolViewController.m
@@ -27,30 +27,30 @@
 /** 点击网络请求按钮事件处理 */
 - (IBAction)clickRequestButtonEvent:(UIButton *)sender {
 
-    
-    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
-    
-    
-    [manager POST:@"http://www.baidu.com" parameters:nil constructingBodyWithBlock:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-        
-        NSLog(@"%@",responseObject);
-    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-        NSLog(@"%@",error.localizedDescription);
-    }];
-    
-    
-//    NSURL *url = [NSURL URLWithString:@"http://www.baidu.com"];
-//    NSURLRequest *request = [NSURLRequest requestWithURL:url];
-//
-//    self.session = [[NSURLSession alloc]init];
-//
-//    NSURLSessionDataTask *dataTask = [self.session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-//
-//        NSString *result = [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
-//        NSLog(@"%@",result);
-//
+//    
+//    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+//    
+//    
+//    [manager POST:@"http://www.baidu.com" parameters:nil constructingBodyWithBlock:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+//        
+//        NSLog(@"%@",responseObject);
+//    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+//        NSLog(@"%@",error.localizedDescription);
 //    }];
-//    [dataTask resume];
+    
+    
+    NSURL *url = [NSURL URLWithString:@"http://www.baidu.com"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+//
+    self.session = [NSURLSession sessionWithConfiguration:NSURLSessionConfiguration.defaultSessionConfiguration];
+//
+    NSURLSessionDataTask *dataTask = [self.session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+
+        NSString *result = [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"%@",result);
+
+    }];
+    [dataTask resume];
 
 }
 
