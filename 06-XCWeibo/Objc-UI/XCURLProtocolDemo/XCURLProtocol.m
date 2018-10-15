c35e1b10bd2e8f3e924b3b3092fb78ec2e830246
Alexiuce <alexiuce@qq.com>
2018-10-15 08:43:11 +0800

    refactor(XCURLProtocolDemo): 注册XCURLProtocol协议

--- 06-XCWeibo/Objc-UI/XCURLProtocolDemo/XCURLProtocol.m
+++ 06-XCWeibo/Objc-UI/XCURLProtocolDemo/XCURLProtocol.m
@@ -8,6 +8,13 @@
 
 #import "XCURLProtocol.h"
 
+
+@interface XCURLProtocol ()
+
+@property (nonatomic, strong)NSURLRequest *request;
+
+@end
+
 @implementation XCURLProtocol
 
 
@@ -18,6 +25,7 @@
     }
     
     NSString *scheme = request.URL.scheme;
+    NSLog(@"url scheme == %@",scheme);
     return  [scheme hasPrefix:@"http"] || [scheme hasPrefix:@"https"];
 }
 
@@ -25,6 +33,30 @@
     return request;
 }
 
+
+- (void)startLoading{
+    
+    NSMutableURLRequest *request = [self.request mutableCopy];
+    [NSURLProtocol setProperty:@YES forKey:request.URL.absoluteString inRequest:request];
+    
+    NSURLSessionConfiguration *config = NSURLSessionConfiguration.defaultSessionConfiguration;
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:config];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        if (error) {
+            [self.client URLProtocol:self didFailWithError:error];
+        }else{
+            [self.client URLProtocol:self didLoadData:data];
+        }
+        
+    }];
+    
+    [task resume];
+}
+
+- (void)stopLoading{
+    
+}
 
 
 
