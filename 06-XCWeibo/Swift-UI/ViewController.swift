b46519de0a4b366b0a4ac68a66ee6c0958c812fb
Alexiuce <alexiuce@qq.com>
2018-10-17 08:08:10 +0800

    feat(Swift-UI): 添加Swift 基础UIdemo

---
+++ 06-XCWeibo/Swift-UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift-UI
+//
+//  Created by Alexcai on 2018/10/17.
+//  Copyright © 2018 Alexcai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ 06-XCWeibo/Swift-UI/XCBaseFoundation/XCBaseFoundationController.swift
@@ -0,0 +1,20 @@
+//
+//  XCBaseFoundationController.swift
+//  Swift-UI
+//
+//  Created by Alexcai on 2018/10/17.
+//  Copyright © 2018 Alexcai. All rights reserved.
+//
+
+import UIKit
+
+class XCBaseFoundationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("hello swift")
+        // Do any additional setup after loading the view.
+    }
+
+}
