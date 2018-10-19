0b1e1d1a8a95e8e3d9cd5136a3da582379c7cd94
Alexiuce <alexiuce@qq.com>
2018-10-19 08:11:18 +0800

    feat(XCPromiseDemo): 添加XCPromise demo 示例group

---
+++ 06-XCWeibo/Objc-UI/XCPromiseDemo/XCPromiseViewController.h
@@ -0,0 +1,17 @@
+//
+//  XCPromiseViewController.h
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/10/19.
+//  Copyright © 2018 Alexcai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCPromiseViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
