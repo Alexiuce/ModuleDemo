b335950a7e7ddb28393b5aef5f87136cf7081403
Alexiuce <alexiuce@qq.com>
2018-10-22 08:41:19 +0800

    refactor(XCPromiseDemo): 添加SubPromiseController
    关于Promise Block的内存管理

---
+++ 06-XCWeibo/Objc-UI/XCPromiseDemo/XCSubPromiseViewController.h
@@ -0,0 +1,17 @@
+//
+//  XCSubPromiseViewController.h
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/10/22.
+//  Copyright © 2018 Alexcai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCSubPromiseViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 06-XCWeibo/Objc-UI/XCPromiseDemo/XCSubPromiseViewController.m
@@ -0,0 +1,27 @@
+//
+//  XCSubPromiseViewController.m
+//  Objc-UI
+//
+//  Created by Alexcai on 2018/10/22.
+//  Copyright © 2018 Alexcai. All rights reserved.
+//
+
+#import "XCSubPromiseViewController.h"
+
+
+@interface XCSubPromiseViewController ()
+
+@end
+
+@implementation XCSubPromiseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+- (IBAction)clickButton:(UIButton *)sender {
+}
+
+@end
