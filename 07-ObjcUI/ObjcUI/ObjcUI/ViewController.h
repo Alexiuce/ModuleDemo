357b2bcb76eabb00704cad6188b12293d2e9ebaa
Alexiuce <alexiuce@qq.com>
2018-10-26 08:16:23 +0800

    feat(07-ObjcUI): 添加新Project

---
+++ 07-ObjcUI/ObjcUI/ObjcUI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjcUI
+//
+//  Created by Alexcai on 2018/10/26.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 07-ObjcUI/ObjcUI/ObjcUITests/ObjcUITests.m
@@ -0,0 +1,37 @@
+//
+//  ObjcUITests.m
+//  ObjcUITests
+//
+//  Created by Alexcai on 2018/10/26.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ObjcUITests : XCTestCase
+
+@end
+
+@implementation ObjcUITests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
