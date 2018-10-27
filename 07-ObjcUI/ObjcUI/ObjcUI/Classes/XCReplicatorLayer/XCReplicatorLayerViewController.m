063b6b1b9da0b84a2d7a188ddeaee782596926b6
Alexiuce <alexiuce@qq.com>
2018-10-27 18:45:52 +0800

    refactor(07-ObjceUI): 添加CAReplicatorLayre 测试代码,水平复制10个内容子图层

--- 07-ObjcUI/ObjcUI/ObjcUI/Classes/XCReplicatorLayer/XCReplicatorLayerViewController.m
+++ 07-ObjcUI/ObjcUI/ObjcUI/Classes/XCReplicatorLayer/XCReplicatorLayerViewController.m
@@ -16,10 +16,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    /** 创建拷贝图层*/
     CAReplicatorLayer *repLayer = [CAReplicatorLayer layer];
-    repLayer.position = CGPointMake(10, 100);
-    
-    // Do any additional setup after loading the view.
+    repLayer.position = CGPointMake(10, 150);
+    /** 创建内容图层 */
+    CALayer *childrenLayer = [CALayer layer];
+    childrenLayer.anchorPoint = CGPointZero;
+    childrenLayer.bounds = CGRectMake(0, 0, 20, 20);
+    childrenLayer.backgroundColor = UIColor.redColor.CGColor;
+    [repLayer addSublayer:childrenLayer];
+
+    /** 复制内容图层副本 */
+    repLayer.instanceCount = 10;
+    repLayer.instanceTransform = CATransform3DMakeTranslation(30, 0, 0);
+     
+     
+     [self.view.layer addSublayer:repLayer];
 }
 
 
