94d9f0c084d933f73dca1fb845bcb28d14699795
Alexiuce <alexiuce@qq.com>
2018-10-28 19:15:06 +0800

    refactor(07-ObjcUI): 添加XCUserInfoTool

---
+++ 07-ObjcUI/ObjcUI/ObjcUI/Classes/XCMuiscManage/XCUserInfoTool.h
@@ -0,0 +1,19 @@
+//
+//  XCUserInfoTool.h
+//  ObjcUI
+//
+//  Created by Alexcai on 2018/10/28.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCUserInfoTool : NSObject
+
++ (instanceType)shareTool;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 07-ObjcUI/ObjcUI/ObjcUI/Classes/XCMuiscManage/XCUserInfoTool.m
@@ -0,0 +1,13 @@
+//
+//  XCUserInfoTool.m
+//  ObjcUI
+//
+//  Created by Alexcai on 2018/10/28.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+#import "XCUserInfoTool.h"
+
+@implementation XCUserInfoTool
+
+@end
