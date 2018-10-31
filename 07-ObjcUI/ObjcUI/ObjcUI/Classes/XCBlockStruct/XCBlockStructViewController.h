ad8ef9e36716f589d0d7527f5c5c9d5cbc1db7c7
Alexiuce <alexiuce@qq.com>
2018-10-31 08:09:43 +0800

    feat(07-ObjcUI): 添加XCBlockStruct

---
+++ 07-ObjcUI/ObjcUI/ObjcUI/Classes/XCBlockStruct/XCBlockStructViewController.h
@@ -0,0 +1,17 @@
+//
+//  XCBlockStructViewController.h
+//  ObjcUI
+//
+//  Created by Alexcai on 2018/10/31.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCBlockStructViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 07-ObjcUI/ObjcUI/ObjcUI/Classes/XCBlockStruct/XCBlockStructViewController.m
@@ -0,0 +1,32 @@
+//
+//  XCBlockStructViewController.m
+//  ObjcUI
+//
+//  Created by Alexcai on 2018/10/31.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+#import "XCBlockStructViewController.h"
+
+@interface XCBlockStructViewController ()
+
+@end
+
+@implementation XCBlockStructViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
