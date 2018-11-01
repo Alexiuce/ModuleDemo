c00ea11fef3d1c6832bdb059444a27c0c1c1a3d7
Alexiuce <alexiuce@qq.com>
2018-11-01 08:53:37 +0800

    feat(07-ObjcUI): 添加小红点示例初始化

---
+++ 07-ObjcUI/ObjcUI/ObjcUI/Classes/XCRedDot/XCRedDotViewController.h
@@ -0,0 +1,17 @@
+//
+//  XCRedDotViewController.h
+//  ObjcUI
+//
+//  Created by Alexcai on 2018/11/1.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCRedDotViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 07-ObjcUI/ObjcUI/ObjcUI/Classes/XCRedDot/XCRedDotViewController.m
@@ -0,0 +1,24 @@
+//
+//  XCRedDotViewController.m
+//  ObjcUI
+//
+//  Created by Alexcai on 2018/11/1.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+#import "XCRedDotViewController.h"
+
+@interface XCRedDotViewController ()
+
+@end
+
+@implementation XCRedDotViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+
+@end
