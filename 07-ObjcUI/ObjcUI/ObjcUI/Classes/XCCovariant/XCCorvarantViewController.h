c27c0ddae599a9aac500a42f3c069ede1b37fab7
秀策 <caijinzhu@dong10.com>
2018-11-04 18:18:41 +0800

    refactor(07-ObjcUI): XCCorvarantViewController 添加视图控制器

---
+++ 07-ObjcUI/ObjcUI/ObjcUI/Classes/XCCovariant/XCCorvarantViewController.h
@@ -0,0 +1,17 @@
+//
+//  XCCorvarantViewController.h
+//  ObjcUI
+//
+//  Created by Alexcai on 2018/11/4.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCCorvarantViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
