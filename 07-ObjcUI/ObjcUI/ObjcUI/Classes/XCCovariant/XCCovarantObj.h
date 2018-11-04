7e3e45b9efced28b39649f8bdca33f760ce297d7
秀策 <caijinzhu@dong10.com>
2018-11-04 18:17:53 +0800

    refactor(07-ObjcUI):
    添加XCCovarantObj

---
+++ 07-ObjcUI/ObjcUI/ObjcUI/Classes/XCCovariant/XCCovarantObj.h
@@ -0,0 +1,17 @@
+//
+//  XCCovarantObj.h
+//  ObjcUI
+//
+//  Created by Alexcai on 2018/11/4.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCCovarantObj : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 07-ObjcUI/ObjcUI/ObjcUI/Classes/XCCovariant/XCCovarantObj.m
@@ -0,0 +1,13 @@
+//
+//  XCCovarantObj.m
+//  ObjcUI
+//
+//  Created by Alexcai on 2018/11/4.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+#import "XCCovarantObj.h"
+
+@implementation XCCovarantObj
+
+@end
