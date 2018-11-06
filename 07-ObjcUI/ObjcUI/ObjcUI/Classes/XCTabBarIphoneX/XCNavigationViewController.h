311d662babe997a80863921ceed181735ef75537
alexiuce <alexiuce@qq.com>
2018-11-06 08:09:49 +0800

    refactor(07-ObjcUI): 添加tabBar iPhoneX的适配demo

---
+++ 07-ObjcUI/ObjcUI/ObjcUI/Classes/XCTabBarIphoneX/XCNavigationViewController.h
@@ -0,0 +1,17 @@
+//
+//  XCNavigationViewController.h
+//  ObjcUI
+//
+//  Created by Alexcai on 2018/11/6.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCNavigationViewController : UINavigationController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 07-ObjcUI/ObjcUI/ObjcUI/Classes/XCTabBarIphoneX/XCNavigationViewController.m
@@ -0,0 +1,32 @@
+//
+//  XCNavigationViewController.m
+//  ObjcUI
+//
+//  Created by Alexcai on 2018/11/6.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+#import "XCNavigationViewController.h"
+
+@interface XCNavigationViewController ()
+
+@end
+
+@implementation XCNavigationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
