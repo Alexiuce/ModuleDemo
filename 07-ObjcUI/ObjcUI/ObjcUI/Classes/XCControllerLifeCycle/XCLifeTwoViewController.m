731ec0a21b2e8ea093eb34d6518bbf38ceb66980
alexiuce <alexiuce@qq.com>
2018-11-07 08:17:02 +0800

    refactor(07-ObjcUI): UIViewController 生命周期present

--- 07-ObjcUI/ObjcUI/ObjcUI/Classes/XCControllerLifeCycle/XCLifeTwoViewController.m
+++ 07-ObjcUI/ObjcUI/ObjcUI/Classes/XCControllerLifeCycle/XCLifeTwoViewController.m
@@ -18,15 +18,24 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    NSLog(@"%s",__FUNCTION__);
 }
-*/
+
+
+- (void)viewDidDisappear:(BOOL)animated{
+    [super viewDidDisappear:animated];
+    NSLog(@"%s",__FUNCTION__);
+}
+/**
+ 点击 dismiss 按钮事件
+
+ @param sender 退出按钮
+ */
+- (IBAction)exitViewController:(UIButton *)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
 
 @end
