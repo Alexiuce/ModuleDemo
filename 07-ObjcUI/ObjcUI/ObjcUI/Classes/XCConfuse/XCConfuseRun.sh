68d39d8fe7693ef92878b56bba5443e33cf6930e
alexiuce <alexiuce@qq.com>
2018-11-08 08:39:09 +0800

    refactor(07-ObjcUI): 设置脚本文件的执行权限( sudo chmod 777  脚本文件)

--- 07-ObjcUI/ObjcUI/ObjcUI/Classes/XCConfuse/XCConfuseRun.sh
+++ 07-ObjcUI/ObjcUI/ObjcUI/Classes/XCConfuse/XCConfuseRun.sh
@@ -5,3 +5,8 @@
 #
 #  Created by Alexcai on 2018/11/8.
 #  Copyright © 2018 dongjiu. All rights reserved.
+#  编译执行脚本文件  Builds Phases  -- > Run Script  -- > 添加此角本的路径信息 $PRJECT_DIR/+ 脚本在项目中的路径
+#  以下角本命令会在编译时执行
+echo "<<<< XCConfuseRun.sh >>>>"
+pwd
+
---
+++ 07-ObjcUI/ObjcUI/XCConfuseViewController.h
@@ -0,0 +1,1 @@
+<<<< XCConfuseRun.sh >>>>
