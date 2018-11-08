ada388714535a9b2c31902cb8fd425e7f07ecdd9
alexiuce <alexiuce@qq.com>
2018-11-08 14:37:15 +0800

    refactor(07-ObjcUI): 添加TODO/FIXME 注释脚本

--- 07-ObjcUI/ObjcUI/ObjcUI/Classes/XCConfuse/XCConfuseRun.sh
+++ 07-ObjcUI/ObjcUI/ObjcUI/Classes/XCConfuse/XCConfuseRun.sh
@@ -33,3 +33,8 @@ echo $b
 echo $c
 
 
+
+##  ================ TODO FIXMI
+KEYWORDS="TODO:|FIXME:|\?\?\?:|\!\!\!:"
+find "${SRCROOT}" \( -name "*.h" -or -name "*.m" \) -print0 | xargs -0 egrep --with-filename --line-number --only-matching "($KEYWORDS).*\$" | perl -p -e "s/($KEYWORDS)/ warning: \$1/"
+
--- 07-ObjcUI/ObjcUI/ObjcUI/Classes/XCConfuse/XCConfuseViewController.m
+++ 07-ObjcUI/ObjcUI/ObjcUI/Classes/XCConfuse/XCConfuseViewController.m
@@ -16,17 +16,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    // TODO : HELOD
     // Do any additional setup after loading the view.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)test{
+// TODO: 需要补充
+    
 }
-*/
 
 @end
